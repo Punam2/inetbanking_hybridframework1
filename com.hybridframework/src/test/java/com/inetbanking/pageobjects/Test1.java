f7e1a568eca3f03d0d70564dab622a344bc1d865
Punam2 <104296657+Punam2@users.noreply.github.com>
2022-04-24 16:55:12 +0530

    Update Test1.java

--- com.hybridframework/src/test/java/com/inetbanking/pageobjects/Test1.java
+++ com.hybridframework/src/test/java/com/inetbanking/pageobjects/Test1.java
@@ -5,5 +5,11 @@ public class Test1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("main method");
+		Test t=new Test();
+		t.test();
+	}
+	public void test()
+	{
+		System.out.println("test method");
 	}
 }
